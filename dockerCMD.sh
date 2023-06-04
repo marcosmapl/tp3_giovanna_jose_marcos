a76c45dd8092167fe3a436763ca026d0864af96d
josemateusc <jose.cordova@icomp.ufam.edu.br>
2023-06-04 19:20:36 -0400

    2GB to docker

--- dockerCMD.sh
+++ dockerCMD.sh
@@ -4,7 +4,7 @@
 # sudo docker build . -t tp3
 
 # Rodar o container
-sudo docker run -d --rm --shm-size=1g --name tp3 -p 5433:5432 -p 8888:8888 -v $(pwd)/notebooks/:/app/notebooks -v $(pwd)/datadir/:/app/datadir tp3
+sudo docker run -d --rm --shm-size=2g --name tp3 -p 5433:5432 -p 8888:8888 -v $(pwd)/notebooks/:/app/notebooks -v $(pwd)/datadir/:/app/datadir tp3
 
 # Esperar um pouco para garantir que o container está rodando
 sleep 5
